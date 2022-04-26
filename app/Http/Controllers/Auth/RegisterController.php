7c7a0575e4c0718f2fdd70ff27273a6f58b776d5
Ken <ken.saetern@plumbingmail.com>
2022-04-26 13:12:26 -0700

    store method creates User and authenticates them

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -12,8 +14,22 @@ class RegisterController extends Controller
         return view('auth.register');
     }
 
-    public function store()
+    public function store(Request $request)
     {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'password' => 'required|confirmed',
+            'email' => 'required|email|unique:users,email'
+        ]);
 
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        auth()->attempt($request->only('email','password'));
+
+        return redirect()->route('dashboard');
     }
 }
