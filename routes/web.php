cd63966f3bfad9d03fe59d3a63e7a45c9e435634
Ken <ken.saetern@plumbingmail.com>
2022-04-26 13:12:49 -0700

    Added routes for links in dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,7 +19,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
+
+
 Route::get('/dashboard', [DashboardController::class,'index'])->name('dashboard');
 
 Route::get('/register', [RegisterController::class,'index'])->name('register');
+Route::post('/register', [RegisterController::class,'store']);
+
+
+Route::get('/login', [LoginController::class,'index'])->name('login');
+Route::post('/login', [LoginController::class,'store']);
+
+Route::post('/logout', [LogoutController::class,'store'])->name('logout');
+
